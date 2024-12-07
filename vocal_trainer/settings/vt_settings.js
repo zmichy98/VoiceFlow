6da691a8bbdb71d29f8382cc3a6541677d21715a
GianVecchini <gianluigi.vecchini@mail.polimi.it>
2024-12-07 17:12:23 +0100

    some work

--- vocal_trainer/settings/vt_settings.js
+++ vocal_trainer/settings/vt_settings.js
@@ -230,3 +230,77 @@ document.querySelectorAll('.key').forEach(key => {
     piano.triggerAttackRelease(note, "6n");
   });
 });
+
+/*------------------------ VOCAL RANGE 3 ------------------------*/
+
+/*------------------------ VOCAL GEAR ------------------------*/
+document.addEventListener("DOMContentLoaded", () => {
+  //query the gear buttons
+  const buttons = document.querySelectorAll(".button-gear");
+
+  // Variables to track selected buttons
+  let selectedMask = false;
+  let selectedLaxvox = false;
+  let selectedNone = false;
+  let selectedGear = "No gear selected";
+
+  // event listener on click
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // Toggle the 'selected' and 'activatedgear' classes
+      button.classList.toggle("selected");
+      button.classList.toggle("activatedgear");
+
+      // Update variables based on the button's current state
+      if (button.id === "mask") {
+        selectedMask = button.classList.contains("selected");
+      } else if (button.id === "laxvox") {
+        selectedLaxvox = button.classList.contains("selected");
+      } else if (button.id === "none") {
+        selectedNone = button.classList.contains("selected");
+      }
+
+      // If you select "None of them" the other need to be deselected
+      if (selectedNone) {
+        // Deselect other buttons
+        buttons.forEach((btn) => {
+          if (btn.id !== "none") {
+            btn.classList.remove("selected");
+            btn.classList.remove("activatedgear");
+          }
+        });
+        // update variables
+        selectedMask = false;
+        selectedLaxvox = false;
+
+        //If you select mask or laxvox deselect none of them
+      } else if (selectedMask || selectedLaxvox) {
+        // Deselect "None of them" if any other button is selected
+        const noneButton = document.getElementById("none");
+        noneButton.classList.remove("selected");
+        noneButton.classList.remove("activatedgear");
+        // Update variable
+        selectedNone = false;
+      }
+
+      // Update variable slectedGear
+      if (selectedMask && selectedLaxvox) {
+        selectedGear = "mask and laxvox"
+      } else if (selectedMask) {
+        selectedGear = "mask"
+      } else if (selectedLaxvox) {
+        selectedGear = "lax vox"
+      } else if (selectedNone) {
+        selectedGear = "No gear selected"
+      }
+
+      // Save the selected range to localStorage
+      localStorage.setItem("selectedGear", selectedGear);
+
+      // Log the states (for debugging or tracking purposes)
+      console.log("Mask selected:", selectedMask);
+      console.log("Laxvox selected:", selectedLaxvox);
+      console.log("None selected:", selectedNone);
+    });
+  });
+});

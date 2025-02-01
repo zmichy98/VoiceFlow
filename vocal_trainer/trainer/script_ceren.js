fe5b1b3e1fab004965f11b59e275793e51218413
Michele Zanardi <michele.zanardi@mail.polimi.it>
2025-02-01 11:04:58 +0100

    Update script_ceren.js

--- vocal_trainer/trainer/script_ceren.js
+++ vocal_trainer/trainer/script_ceren.js
@@ -560,7 +560,7 @@ async function setLoginValues() {
     if(logged === true) {
         const accountsRef = db.collection("store").doc("accounts");
         // Creiamo l'array di dati dell'utente
-        const userData = [nickname, password, 0, 0, 0, experience, time, range, gear, manual, firstmanNote, secondmanNote];
+        const userData = [nickname, password, 0, 0, 0, experience, time, range, gear, manual, firstmanNote, secondmanNote, laxVox, mask];
 
         // Aggiorna Firestore con il nuovo array senza sovrascrivere
         const doc = await accountsRef.get();
